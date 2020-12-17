0c3e0907f8ea4710c47bd8012a999be7d9100362
Corey Van Splinter <cvanspl1@gmail.com>
2020-12-17 15:13:37 -0500

    updated styling

--- client/components/Home1.jsx
+++ client/components/Home1.jsx
@@ -1,7 +1,6 @@
 import React, {Component, useState, useEffect} from 'react';
 //react-router-dom
 import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import '../styles/Home1.scss';
 
 export default function Home(){
     return(
