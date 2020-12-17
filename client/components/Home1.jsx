4900f3c3a27341171256ab990475b89fc18b24a6
Corey Van Splinter <cvanspl1@gmail.com>
2020-12-16 23:13:54 -0500

    Added home screen styling

--- client/components/Home1.jsx
+++ client/components/Home1.jsx
@@ -1,6 +1,7 @@
 import React, {Component, useState, useEffect} from 'react';
 //react-router-dom
 import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import '../styles/Home1.scss';
 
 export default function Home(){
     return(
